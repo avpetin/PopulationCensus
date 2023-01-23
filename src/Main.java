71b05f4dd0972202683eeed364ea1f8704434d4e
Alexey Petin <alex_beholder@mail.ru>
2023-01-23 22:56:57 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+    }
+}
